db78d71219dbf9ff9008c9ee87afc77f655fad6f
kd47311010 <kd47311010@gmail.com>
2015-01-21 12:35:48 +0900

    Initial Commit

---
+++ app/src/main/java/com/home/movieviewer/MovieList.java
@@ -0,0 +1,18 @@
+package com.home.movieviewer;
+
+import android.net.Uri;
+
+/**
+ * Created by 88 on 2015-01-14.
+ */
+public class MovieList {
+    private static String BASE_URL
+            = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json";
+
+    public static String getUrl() {
+        return Uri.parse(BASE_URL).buildUpon()
+                .appendQueryParameter(Config.PARAM_KEY, Config.API_KEY)
+                .build().toString();
+    }
+}
+
---
+++ app/src/main/java/com/home/movieviewer/VideoBrowseActivity.java
@@ -0,0 +1,8 @@
+package com.home.movieviewer;
+
+/**
+ * Created by A150 on 2015-01-21.
+ */
+public class VideoBrowseActivity {
+
+}
