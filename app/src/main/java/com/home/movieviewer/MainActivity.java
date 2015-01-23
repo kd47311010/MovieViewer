9fbfe6dad44b1b29efcad899a1ed5c3ac2cc45eb
kd47311010 <kd47311010@gmail.com>
2015-01-23 17:19:20 +0900

    SwipeRefreshLayout
    make SwipeRefreshLayout

--- app/src/main/java/com/home/movieviewer/MainActivity.java
+++ app/src/main/java/com/home/movieviewer/MainActivity.java
@@ -1,15 +1,23 @@
 package com.home.movieviewer;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
 import java.io.IOException;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -24,7 +32,57 @@ public class MainActivity extends ActionBarActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        final SwipeRefreshLayout swipeView = (SwipeRefreshLayout) findViewById(R.id.swipe);
+        final TextView rndNum = (TextView) findViewById(R.id.rndNum);
+        swipeView.setColorSchemeColors(
+                android.R.color.holo_blue_light,
+                android.R.color.holo_blue_light,
+                android.R.color.holo_green_light,
+                android.R.color.holo_green_light);
+
+        swipeRefreshLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.swipeLayout);
+        swipeRefreshLayout.setColorSchemeColors(getResources().getColor(R.color.colorAccent));
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+
+
+            //swipeView.setEnabled(false);
+            //ListView lView = (ListView) findViewById(R.id.list);
+            //ArrayAdapter<String> adp = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, createItems(40,0 ));
+            //lView.setAdapter(adp);
+
+            //swipeView.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+
+            @Override
+            public void onRefresh() {
+                swipeView.setRefreshing(true);
+                Log.d("Swipe", "Refreshing Number");
+                (new Handler()).postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        swipeView.setRefreshing(false);
+                        double f = Math.random();
+                        rndNum.setText(String.valueOf(f));
+                    }
+                }, 3000);
+            }
+        });
+
+        lView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView absListView, int i) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView absListView, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if (firstVisibleItem == 0)
+                    swipeView.setEnabled(true);
+                else
+                    swipeView.setEnabled(false);
+            }
+        });
     }
+
 
     private String getData(String url) throws IOException {
         OkHttpClient okHttpClient = new OkHttpClient();
