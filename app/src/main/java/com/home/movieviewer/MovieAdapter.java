d0d6d04e30c40b319d8852a7610803141b571961
NamHyun, Gu <mnhan0403@gmail.com>
2015-03-06 10:57:49 +0900

    Change Layout

--- app/src/main/java/com/home/movieviewer/MovieAdapter.java
+++ app/src/main/java/com/home/movieviewer/MovieAdapter.java
@@ -1,6 +1,10 @@
 package com.home.movieviewer;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,11 +12,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeoutException;
 
 /**
  * Created by P400 on 2015-01-29.
@@ -65,22 +66,39 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         View itemView = holder.getView();
         TextView primaryTextView = (TextView) itemView.findViewById(R.id.content_primary);
         TextView secondaryTextView = (TextView) itemView.findViewById(R.id.content_secondary);
-        TextView threeTextView = (TextView) itemView.findViewById(R.id.content_three);
+        TextView rankTextView = (TextView) itemView.findViewById(R.id.content_rank);
         ImageView thumbnailView = (ImageView) itemView.findViewById(R.id.content_image);
+        ImageView rankImageView = (ImageView) itemView.findViewById(R.id.content_rank_background);
 
-        String primaryText = new StringBuilder().append(container.getRank()).append(" ").append(container.getMovieNm())
+        String primaryText = new StringBuilder().append(" ").append(container.getMovieNm())
                 .append(" (").append(container.getOpenDt().substring(0, 4)).append(")").toString();
 
         String secondaryText = new StringBuilder()
+                .append("누적 " + container.getAudiAcc() + " 명")
                 .append(" ( ").append(container.getRankInten()).append(" 위 ) ").toString();
-
-        String threeText = new StringBuilder()
-                .append(" ( 누적 ").append(container.getAudiAcc()).append("명 )").toString();
 
         primaryTextView.setText(primaryText);
         secondaryTextView.setText(secondaryText);
-        threeTextView.setText(threeText);
+        rankTextView.setText(container.getRank());
         thumbnailView.setImageDrawable(mActivity.getResources().getDrawable(R.drawable.ic_action_turned_in_not));
+        setRankImage(rankImageView);
+    }
+
+    private void setRankImage(ImageView view) {
+        Bitmap bitmap = null;
+        view.setColorFilter(mActivity.getResources().getColor(R.color.colorAccent));
+        if(view.getDrawable() instanceof  BitmapDrawable) {
+            bitmap = ((BitmapDrawable) view.getDrawable()).getBitmap();
+            setCircleImage(bitmap, view);
+        }
+    }
+
+    private void setCircleImage(Bitmap bitmap, ImageView view) {
+        RoundedBitmapDrawable bitmapDrawable =
+                RoundedBitmapDrawableFactory.create(mActivity.getResources(), bitmap);
+        bitmapDrawable.setCornerRadius(Math.max(bitmap.getWidth(), bitmap.getHeight()) / 2.0f);
+        bitmapDrawable.setAntiAlias(true);
+        view.setImageDrawable(bitmapDrawable);
     }
 
 
